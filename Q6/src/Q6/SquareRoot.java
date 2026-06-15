21efb5d02358dbc2d3c1f04f93853dc59d65cefe
Ram <ramanmmm8082@gmail.com>
2026-06-15 19:59:26 +0530

    feat : new programs

---
+++ Q6/src/Q6/SquareRoot.java
@@ -0,0 +1,12 @@
+package Q6;
+
+public class SquareRoot {
+        public static void main(String[] args) {
+            int num = 25;
+
+            double result = Math.sqrt(num);
+
+            System.out.println("Square Root = " + result);
+        }
+    }
+
